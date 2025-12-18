9aafc562d4b117da8d8e7aa9455a194eabf714bc
Amethyst <439291857@qq.com>
2025-12-18 07:42:33 +0000

    fix grpc context

--- src/grpc_ps/grpc_ps_client.cpp
+++ src/grpc_ps/grpc_ps_client.cpp
@@ -94,12 +94,14 @@ int GRPCParameterClient::GetParameter(const base::ConstArray<uint64_t>& keys,
   get_param_requests_.clear();
   get_param_responses_.clear();
   get_param_resonse_readers_.clear();
+  get_param_contexts_.clear();
 
   int request_num =
       (keys.Size() + MAX_PARAMETER_BATCH - 1) / MAX_PARAMETER_BATCH;
   get_param_status_.resize(request_num);
   get_param_requests_.resize(request_num);
   get_param_responses_.resize(request_num);
+  get_param_contexts_.resize(request_num);
 
   for (int start = 0, index = 0; start < keys.Size();
        start += MAX_PARAMETER_BATCH, ++index) {
@@ -111,10 +113,13 @@ int GRPCParameterClient::GetParameter(const base::ConstArray<uint64_t>& keys,
     request.set_keys(reinterpret_cast<const char*>(&keys[start]),
                      sizeof(uint64_t) * key_size);
     // rpc
-    grpc::ClientContext context;
+    // grpc::ClientContext context;
+    if(!get_param_contexts_[index]) {
+      get_param_contexts_[index] = std::make_unique<grpc::ClientContext>();
+    }
     std::unique_ptr<ClientAsyncResponseReader<GetParameterResponse>> rpc =
-        stubs_[0]->AsyncGetParameter(&context, request, &cq);
-    rpc->Finish(&response, &status, reinterpret_cast<void*>(index));
+        stubs_[0]->AsyncGetParameter(get_param_contexts_[index].get(), request, &cq);
+    rpc->Finish(&response, &status, reinterpret_cast<void *>(index));
   }
   int get = 0;
   while (get != request_num) {
@@ -176,6 +181,7 @@ int GRPCParameterClient::GetParameter(const base::ConstArray<uint64_t>& keys,
   get_param_requests_.clear();
   get_param_responses_.clear();
   get_param_resonse_readers_.clear();
+  get_param_contexts_.clear();
 
   values->reserve(keys.Size());
 
@@ -185,6 +191,7 @@ int GRPCParameterClient::GetParameter(const base::ConstArray<uint64_t>& keys,
   get_param_status_.resize(request_num);
   get_param_requests_.resize(request_num);
   get_param_responses_.resize(request_num);
+  get_param_contexts_.resize(request_num);
 
   for (int start = 0, index = 0; start < keys.Size();
        start += MAX_PARAMETER_BATCH, ++index) {
@@ -196,10 +203,13 @@ int GRPCParameterClient::GetParameter(const base::ConstArray<uint64_t>& keys,
     request.set_keys(reinterpret_cast<const char*>(&keys[start]),
                      sizeof(uint64_t) * key_size);
     // rpc
-    grpc::ClientContext context;
+    // grpc::ClientContext context;
+    if(!get_param_contexts_[index]) {
+      get_param_contexts_[index] = std::make_unique<grpc::ClientContext>();
+    }
     get_param_resonse_readers_.emplace_back(
-        stubs_[0]->AsyncGetParameter(&context, request, &cq));
-    auto& rpc = get_param_resonse_readers_.back();
+        stubs_[0]->AsyncGetParameter(get_param_contexts_[index].get(), request, &cq));
+    auto &rpc = get_param_resonse_readers_.back();
     // GetParameter(&context, request, &response);
     rpc->Finish(&response, &status, reinterpret_cast<void*>(index));
   }
@@ -253,18 +263,19 @@ GRPCParameterClient::PrefetchParameter(const base::ConstArray<uint64_t>& keys) {
        start += MAX_PARAMETER_BATCH, ++index) {
     int key_size = std::min((int)(keys.Size() - start), MAX_PARAMETER_BATCH);
     pb.key_sizes_[index] = key_size;
-    auto& status         = pb.status_[index];
-    if (!pb.contexts_[index]) {
+    auto &status = pb.status_[index];
+    if(!pb.contexts_[index]) {
       pb.contexts_[index] = std::make_unique<grpc::ClientContext>();
     }
-    auto& request  = pb.requests_[index];
-    auto& response = pb.responses_[index];
-    request.set_keys(reinterpret_cast<const char*>(&keys[start]),
-                     sizeof(uint64_t) * key_size);
+    auto &request = pb.requests_[index];
+    auto &response = pb.responses_[index];
+    request.set_keys(reinterpret_cast<const char *>(&keys[start]),
+                    sizeof(uint64_t) * key_size);
     // rpc
-    pb.response_readers_.emplace_back(stubs_[0]->AsyncGetParameter(
-        pb.contexts_[index].get(), request, pb.cqs_.get()));
-    auto& rpc = pb.response_readers_.back();
+    // grpc::ClientContext context;
+    pb.response_readers_.emplace_back(
+        stubs_[0]->AsyncGetParameter(pb.contexts_[index].get(), request, pb.cqs_.get()));
+    auto &rpc = pb.response_readers_.back();
     // GetParameter(&context, request, &response);
     rpc->Finish(&response, &status, reinterpret_cast<void*>(index));
   }
@@ -530,6 +541,9 @@ int GRPCParameterClient::PutParameter(
     const std::vector<std::vector<float>>& values) {
   std::vector<uint64_t> key_vec(keys.Data(), keys.Data() + keys.Size());
   bool success = PutParameter(key_vec, values);
+  if (!success) {
+    LOG(ERROR) << "PutParameter batch failed";
+  }
   return success ? 1 : 0;
 }
 
